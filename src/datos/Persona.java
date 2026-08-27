3d09f0833eb0abe1356ae23c75d192b9bc9b7a3b
Marco Pappalardo <marco.pappalardou2003qgmail.com>
2026-08-27 17:40:47 -0300

    Clase persona, persona dao y persona abm

--- src/datos/Persona.java
+++ src/datos/Persona.java
@@ -1,14 +1,135 @@
 package datos;
 
 import java.time.LocalDate;
+import java.util.Objects;
+
+import datos.Persona;
 
 public class Persona {
-	protected int idPersona;
+	
 	protected String nombre;
 	protected String apellido;
 	protected long dni;
 	protected LocalDate fechaNacimiento;
 	protected LocalDate fechaIngreso;
 	protected double sueldoBase;
+	protected int idPersona;
+	
+	
+	public Persona() {
+		
+	}
+	
+	public Persona(String nombre, String apellido, long dni, LocalDate fechaNacimiento, LocalDate fechaIngreso,
+			double sueldoBase) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.fechaNacimiento = fechaNacimiento;
+		this.fechaIngreso = fechaIngreso;
+		this.sueldoBase = sueldoBase;
+	}
+
+	
+	/*public Persona traer(int dni) {
+		int i=0;
+		Persona personaBuscada=null;
+		while(i<this.lstPersonas.size()&&personaBuscada==null) {
+			if(this.lstPersonas.get(i).getDni()==dni) {
+				personaBuscada=this.lstPersonas.get(i);
+			}
+			i++;
+		}
+		return personaBuscada;
+		
+		
+	}*/
+	
+	/*public Persona traer(LocalDate fecha) {
+		int i=0;
+		Persona personaBuscada=null;
+		while(i<this.lstPersonas.size()&&personaBuscada==null) {
+			if(this.lstPersonas.get(i).getFechaNacimiento().isEquals(fecha) {
+				personaBuscada=this.lstPersonas.get(i);
+			}
+			i++;
+		}
+		return personaBuscada;
+		
+		
+	}*/
+	
+	public int getIdPersona() {
+		return idPersona;
+	}
+	public void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public long getDni() {
+		return dni;
+	}
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", fechaNacimiento="
+				+ fechaNacimiento + ", fechaIngreso=" + fechaIngreso + ", sueldoBase=" + sueldoBase + ", idPersona="
+				+ idPersona + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellido, dni, fechaIngreso, fechaNacimiento, idPersona, nombre, sueldoBase);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(apellido, other.apellido) && dni == other.dni
+				&& Objects.equals(fechaIngreso, other.fechaIngreso)
+				&& Objects.equals(fechaNacimiento, other.fechaNacimiento) && idPersona == other.idPersona
+				&& Objects.equals(nombre, other.nombre)
+				&& Double.doubleToLongBits(sueldoBase) == Double.doubleToLongBits(other.sueldoBase);
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public LocalDate getFechaIngreso() {
+		return fechaIngreso;
+	}
+	public void setFechaIngreso(LocalDate fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+	public double getSueldoBase() {
+		return sueldoBase;
+	}
+	public void setSueldoBase(double sueldoBase) {
+		this.sueldoBase = sueldoBase;
+	}
+	
 
 }
---
+++ src/negocio/PersonaABM.java
@@ -0,0 +1,57 @@
+package negocio;
+import java.time.LocalDate;
+import java.util.List;
+import dao.PersonaDao;
+import datos.Persona;
+public class PersonaABM {
+PersonaDao dao = new PersonaDao();
+
+public Persona traer(long idPersona) {
+return dao.traer(idPersona);
+}
+
+public Persona traer(int dni) {
+return dao.traer(dni);
+}
+
+protected String nombre;
+protected String apellido;
+protected long dni;
+
+protected LocalDate fechaNacimiento;
+protected LocalDate fechaIngreso;
+protected double sueldoBase;
+protected int idPersona;
+
+public int agregar(String apellido, String nombre, long dni, LocalDate fechaNacimiento,LocalDate fechaIngreso,double sueldoBase)throws Exception {
+	
+	Persona pe = dao.traer(dni);
+	if (pe != null) {
+		throw new Exception("ya existe este cliente en la BD");
+	}
+Persona p = new Persona( nombre, apellido, dni,fechaNacimiento, fechaIngreso, sueldoBase);
+return dao.agregar(p);
+}
+
+
+/*public void modificar(Persona p) {
+/*
+* En caso de editar el dni, antes de actualizar, validar que no exista un cliente con el mismo
+dni y si eso pasa lanzar la Exception
+
+dao.actualizar(c);
+}*/
+
+public void eliminar(long idPersona)throws Exception {
+
+Persona p = dao.traer(idPersona);
+if (p != null) {
+	throw new Exception("Esta persona no existe");
+}
+dao.eliminar(p);
+}
+
+public List<Persona> traer() {
+return dao.traer();
+}
+}
