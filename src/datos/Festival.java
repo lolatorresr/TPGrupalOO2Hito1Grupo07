458b69e5a68f8766941a48e2e884b38c204d1220
Lola Torres Rodriguez <lolatorresrodr@gmail.com>
2026-08-31 12:12:26 -0300

    clases java festival y foodtruck

--- src/datos/Festival.java
+++ src/datos/Festival.java
@@ -1,5 +1,81 @@
 package datos;
 
+import java.time.LocalDate;
+
 public class Festival {
+	private int idFestival;
+	private String nombre;
+	private String temporada;
+	private LocalDate fechaInicio;
+	private LocalDate fechaFin;
+	private UnidadDeVenta unidades;
+	
+	public Festival() {
+	}
+
+	public Festival(String nombre, String temporada, LocalDate fechaInicio, LocalDate fechaFin,
+			UnidadDeVenta unidades) {
+		this.nombre = nombre;
+		this.temporada = temporada;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.unidades = unidades;
+	}
+
+	public int getIdFestival() {
+		return idFestival;
+	}
+
+	protected void setIdFestival(int idFestival) {
+		this.idFestival = idFestival;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTemporada() {
+		return temporada;
+	}
+
+	public void setTemporada(String temporada) {
+		this.temporada = temporada;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public UnidadDeVenta getUnidades() {
+		return unidades;
+	}
+
+	public void setUnidades(UnidadDeVenta unidades) {
+		this.unidades = unidades;
+	}
+
+	@Override
+	public String toString() {
+		return "Festival [idFestival=" + idFestival + ", nombre=" + nombre + ", temporada=" + temporada
+				+ ", fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + ", unidades=" + unidades + "]";
+	}
+	
+	
 
 }
--- src/datos/FoodTruck.java
+++ src/datos/FoodTruck.java
@@ -1,5 +1,42 @@
 package datos;
 
-public class FoodTruck {
+public class FoodTruck extends UnidadDeVenta{
+	private String patente;
+	private boolean conexionElectrica;
+	
+	public FoodTruck() {
+	}
+
+	public FoodTruck(String nombreComercial, Persona responsable, double superficie, String codigoUnico,
+			String patente, boolean conexionElectrica) {
+		super(nombreComercial, responsable, superficie, codigoUnico);
+		this.patente=patente;
+		this.conexionElectrica=conexionElectrica;
+	}
+
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public boolean isConexionElectrica() {
+		return conexionElectrica;
+	}
+
+	public void setConexionElectrica(boolean conexionElectrica) {
+		this.conexionElectrica = conexionElectrica;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " | FoodTruck [patente=" + patente + ", conexionElectrica=" + conexionElectrica + "]";
+	}
+	
+	
+	
+	
 
 }
